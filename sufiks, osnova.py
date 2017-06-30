6b07e70b82d889b6b6851b96d8c0fdcb6a9d260e
DijanaMarkovic <dijana.m.1406@gmail.com>
2017-06-30 17:02:49 +0200

    Add files via upload

---
+++ sufiks, osnova.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 16:25:54 2017
+
+@author: Korsnik
+"""
+
+rec1 = "pevac"
+rec_osnova1 = rec1 [0:3]
+print(rec_osnova1)
+rec_sufiks1= rec1 [3:5]
+print(rec_sufiks1)
+
+rec2 = "beogradski"
+rec_osnova2 = rec2 [-10:-3]
+print(rec_osnova2)
+rec_sufiks2 = rec2 [-3:10]
+print(rec_sufiks2)
+
+rec3 = "spavanje"
+rec_osnova3 = rec3 [0:5]
+print(rec_osnova3)
