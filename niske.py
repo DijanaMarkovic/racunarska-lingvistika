cd206e5a72d9ba7a5951a335108dffa14a9247a7
DijanaMarkovic <dijana.m.1406@gmail.com>
2017-06-30 16:25:18 +0200

    Add files via upload

---
+++ niske.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 15:43:13 2017
+
+@author: Korsnik
+"""
+
+tekst = "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+tekst = tekst.lower()
+lista_reči = tekst.split (" ")
+print (lista_reči)
+
+niska = "Dokle ću više biti niska!"
+karakteri = list(niska)
+print(karakteri)
