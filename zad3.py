64e31e6f27238f09166ae6d8d63801f1d8e7d349
DijanaMarkovic <dijana.m.1406@gmail.com>
2017-07-01 17:44:42 +0200

    Add files via upload

---
+++ zad3.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+petnica = {"A": ["ANT", "AHL", "IST", "DIZ", "DRH", "LIN", "PSI"], "B": ["BIO", "BMD", "EBH", "HEM", "GEO"], "C": ["AST", "FIZ", "MAT", "PFE", "RAČ", "TEH"]}
+#[]
+print(petnica["A"])
+print(petnica["B"])
+print(petnica["C"])
